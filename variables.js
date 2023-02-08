2a72e09c7c7bf59f0fc5bd1cb7fff93729cd65e9
MosK <118230704+MosK1985@users.noreply.github.com>
2023-02-08 00:12:47 -0300

    Tarea 1 de Modulo 3

---
+++ variables.js
@@ -0,0 +1,18 @@
+// string
+var bootcamp = 'henry';
+
+// Numeros
+var numeroentero = 17;
+var numeronegativo = -4;
+var numerodecimal =  11.75;
+
+//booleanos
+var estoyentendiendo = true;
+var estoyaburrido = false;
+
+// undefined
+var cajon1;
+console.log (cajon1);
+
+//null
+var sinnada = null;
