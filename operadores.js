e4d90eceaa28f38f20a3716a5fa3ff278e03ae9f
MosK <118230704+MosK1985@users.noreply.github.com>
2023-02-08 01:26:07 -0300

    Tarea 2 Modulo 3

---
+++ operadores.js
@@ -0,0 +1,11 @@
+console.log(3 + 5);
+console.log(145 - 67);
+console.log(12 * 3);
+console.log(59 / 4);
+console.log(165 % 7);
+console.log(6 + 3 * 2 - 6);
+console.log((6 + 3) * 2 - 6);
+console.log('hola ' + 'mosk');
+var nombre = 'mosk';
+var saludo = 'hola ' + nombre;
+console.log(saludo)
