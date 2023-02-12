f0b7ed4e1e3091b0276473b03c282fe1af00d490
MosK <118230704+MosK1985@users.noreply.github.com>
2023-02-11 22:29:10 -0300

    Tares del modulo 3 operadores y funciones

---
+++ Funciones2.js
@@ -0,0 +1,17 @@
+function CuidadoConElConsoleLog(nombre) {
+    console.log(nombre);
+    return nombre;
+    }
+
+function otrafuncion() {
+    return (
+        "El nombre retornadopor la funcion 'CuidadoConElConsoleLog' es: " + CuidadoConElConsoleLog("MosK") 
+
+    );
+}  
+
+function CuidadoConElConsoleLog(nombre) {
+    return nombre;
+    console.log(nombre);
+    }
+ 
--- funciones .js
+++ funciones .js
@@ -3,3 +3,22 @@ function sumaTres(x) {
 }
 
 sumaTres(5);
+
+// PRIMERO
+function sumaTres(x) {
+    return x + 3;
+}
+
+// SEGUNDO
+var sumaTres = function (x) {
+    return x + 3;
+};
+
+// TERCERO
+var sumaTres = (x) => {
+    return x + 3;
+};
+
+var animal = 'caballo'
+
+console.log (animal);
