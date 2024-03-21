a90887cf6ee00115c701a32a2e76999f6cdc6690
MosK <118230704+MosK1985@users.noreply.github.com>
2024-03-21 19:39:23 -0300

    ultima actualizacion prep course

--- arrays.js
+++ arrays.js
@@ -3,3 +3,12 @@ listaDeCompras[3] = 'tomates';
 listaDeCompras[1] = 'lechuga';
 
 console.log(listaDeCompras.length);
+
+// Ver lista de compras
+var elementoDelArray = listaDeCompras[1];
+console.log(elementoDelArray);
+
+
+// Lenght
+var nombres = ['Matias', 'Maria', 'Diego', 'Rosa'].length;
+console.log(nombres);
--- metodosarrays.js
+++ metodosarrays.js
@@ -6,10 +6,35 @@ colores.shift();
 
 
 
-
+// INCLUDES
  let pintores = ['Picasso', 'Velazquez', 'Van Gogh', 'Dali'];
  let incluyeDali = pintores.includes('Dali');
 
-
- let numeros = [1, 6, 8, 9];
+// EVERY
+ let numeros1 = [1, 6, 8, 9];
  let cumplenCondicion = numeros.every((num) => {return num > 5})
+
+
+// SPLIT
+var palabra = 'Henri';
+var palabraSeparada = palabra.split('');
+
+console.log(palabraSeparada);
+
+
+// JOIN
+var palabraArreglada = palabraSeparada.join('');
+
+console.log(palabraArreglada);
+
+
+// FOREACH
+var numeros = [ 1, 2, 3, 4 ];
+numeros.forEach( (num) => { console.log(num) } )
+
+
+// MAP
+var numeros = [ 1, 2, 3, 4 ];
+var masUno = numeros.map( (num) => { return num + 1 } );
+
+console.log(masUno);
